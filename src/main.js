96d6fd8aaa6402beec0b19ae18ea1b18ef804f09
Jack Mattar <jackeline_mattar@hotmail.com>
2019-11-18 15:55:52 -0300

    adicionando main.js

--- src/main.js
+++ src/main.js
@@ -11,23 +11,6 @@ function locationHashChanged() {
         } else if (window.location.hash === '#signup') {
           document.querySelector('main').innerHTML = Signup();
         }
-
-  // firebase.auth().onAuthStateChanged((user) => {
-  //   if (user) {  
-  //     if (window.location.hash === '#home') {
-  //       document.querySelector('main').innerHTML = Home();
-  //     } else if (window.location.hash === '#login' || window.location.hash === '') {
-  //       document.querySelector('main').innerHTML = Login();
-  //     } else if (window.location.hash === '#signup') {
-  //       document.querySelector('main').innerHTML = Signup();
-  //   } else if (!user) {
-  //     if (window.location.hash === '#login' || window.location.hash === '') {
-  //       document.querySelector('main').innerHTML = Login();
-  //     } else if (window.location.hash === '#signup') {
-  //       document.querySelector('main').innerHTML = Signup();
-  //     }
-  //   }
-  // });
 }
 window.addEventListener('load', locationHashChanged);
 window.addEventListener('hashchange', locationHashChanged, false);
